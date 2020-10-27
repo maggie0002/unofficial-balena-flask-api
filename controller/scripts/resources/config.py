a718fa911430c7701c99c278b5d0d958f080bd96
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2020-10-27 09:47:33 -0700

    Delete config.py

--- controller/scripts/resources/config.py
+++
@@ -1,4 +0,0 @@
-# #Required variables to be set by user#
-deafultssid = 'choose-a-default-ssid-here'
-defaulthostname = 'put-device-hostname-you-built-your-device-with-here'
-#####################################
