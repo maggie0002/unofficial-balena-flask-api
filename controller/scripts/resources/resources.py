3d2de0132fade486b3c7c752248c873e4cc6cfbb
maggie0002 <arran.magee@icloud.com>
2020-11-22 12:16:39 -0800

    Adjust JSON returned by Flask

--- controller/scripts/resources/resources.py
+++ controller/scripts/resources/resources.py
@@ -1,4 +1,4 @@
-from flask_restful import abort, Resource
+from flask_restful import Resource
 from werkzeug import serving
 from resources.processes import curl, wifi
 import resources.globals
@@ -19,14 +19,15 @@ class wificonnectionstatus(Resource):
         response = wifi().checkconnection()
 
         if response:
-            return {'wificonnectionstatus': 'Connected', 'status': 200}, 200
+            return {'status': 200, 'message': 'connected'}, 200
         else:
-            return {'wificonnectionstatus': 'Not connected', 'status': 206}, 206
+            return {'status': 206, 'message': 'disconnected'}, 206
 
 class device(Resource):
     def get(self):
 
-        response = curl(method = "get", path = "/v1/device?apikey=")
+        response = curl(method = "get", 
+                        path = "/v1/device?apikey=")
 
         return response["jsonresponse"], response["status_code"]
 
@@ -34,38 +35,43 @@ class healthcheck(Resource):
     def get(self):
         
         serving.WSGIRequestHandler.log_request = log_request
-        return {'healthcheck': 'ok', 'status': 200}, 200
+        return {'status': 200, 'message': 'ok'}, 200
 
 class hostconfig(Resource):
     def get(self, hostname):
 
-        try:
-            hostname
-        except NameError:
-            abort(404, hostconfig='No hostname entered.', status=404)
+        response = curl(method = "patch", 
+                        path = "/v1/device/host-config?apikey=", 
+                        string = '{"network": {"hostname": "%s"}}'%(hostname))
 
-        response = curl(method = "patch", path = "/v1/device/host-config?apikey=", string = '{"network": {"hostname": "%s"}}'%(hostname))
-
-        return {'hostconfig': response["text"], 'status': response["status_code"]}, response["status_code"]
+        return {
+            'status': response["status_code"],
+            'hostname': hostname,
+            'message': response["text"]
+        }, response["status_code"]
 
 class journallogs(Resource):
     def get(self):
 
-        response = curl(method = "post", path  = "/v2/journal-logs?apikey=", string = '("follow", "false", "all", "true", "format", "short")')
+        response = curl(method = "post", 
+                        path  = "/v2/journal-logs?apikey=", 
+                        string = '("follow", "false", "all", "true", "format", "short")')
 
         return response["text"], response["status_code"]
 
 class update(Resource):
     def get(self):
 
-        response = curl(method ="post", path = "/v1/update?apikey=", string = '("force", "true")')
+        response = curl(method ="post",
+                        path = "/v1/update?apikey=", 
+                        string = '("force", "true")')
 
-        return {'update': response["text"], 'status': response["status_code"]}, response["status_code"]
+        return {'status': response["status_code"], 'message': response["text"]}, response["status_code"]
 
 class uuid(Resource):
     def get(self):
 
-        return {'uuid': resources.globals.BALENA_DEVICE_UUID}, 200
+        return {'uuid': resources.globals.BALENA_DEVICE_UUID}
 
 class wififorget(Resource):
     def get(self):
@@ -75,12 +81,16 @@ class wififorget(Resource):
 
         #If the device is connected to a wifi network
         if not connectionstate:
-            abort(409, wififorget='The device is not connected. No action taken.', status=409) 
+
+            return {
+                'status': 409,
+                'message': 'Device is already disconnected, connection cannot be reset.'
+            }, 409
 
         wififorget = threading.Thread(target=wifi.forget, name='wififorget')
         wififorget.start()
 
-        return {'wififorget': 'Reset request sent.', 'status': 202}, 202  
+        return {'status': 202, 'message': 'Reset request sent.'}, 202  
 
 class wififorgetall(Resource):
     def get(self):
@@ -88,4 +98,4 @@ class wififorgetall(Resource):
         wififorgetall = threading.Thread(target=wifi.forgetall, name='wififorgetall')
         wififorgetall.start()
 
-        return {'wififorgetall': 'Reset request sent.', 'status': 202}, 202
+        return {'status': 202, 'message': 'Reset request sent.'}, 202
